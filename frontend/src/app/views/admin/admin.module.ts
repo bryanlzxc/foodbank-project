f05c3da80ac7bb3e6d25968aafc88b5fd74279e4
Louis Lim Ying Wei <gamemaniaz@hotmail.com>
2017-10-24 08:24:47 +0800

    wrapper for pages and admin dashboard grid

--- frontend/src/app/views/admin/admin.module.ts
+++ frontend/src/app/views/admin/admin.module.ts
@@ -1,16 +1,21 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { FormsModule } from "@angular/forms";
-import { RouterModule } from "@angular/router";
-import { AdminRoutes } from './admin.routing';
-
-import { AdminDashboardComponent } from './dashboard/dashboard.component';
-import { AdminInventoryComponent } from './inventory/inventory.component';
+import { NgModule }                 from '@angular/core';
+import { CommonModule }             from '@angular/common';
+import { FormsModule }              from '@angular/forms';
+import { RouterModule }             from '@angular/router';
+import { AdminRoutes }              from './admin.routing';
+import {
+    MatCardModule,
+    MatGridListModule
+}                                   from '@angular/material';
+import { AdminDashboardComponent }  from './dashboard/dashboard.component';
+import { AdminInventoryComponent }  from './inventory/inventory.component';
 
 @NgModule({
     imports: [
         CommonModule,
         FormsModule,
+        MatCardModule,
+        MatGridListModule,
         RouterModule.forChild(AdminRoutes)
     ],
     declarations: [
