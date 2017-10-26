eb47d0eeb29c7b1bac441a062111899454601a6c
Louis Lim Ying Wei <gamemaniaz@hotmail.com>
2017-10-26 09:31:23 +0800

    navigation via navbar

--- frontend/src/app/components/common/navigation/navigation.component.ts
+++ frontend/src/app/components/common/navigation/navigation.component.ts
@@ -1,6 +1,7 @@
-import { Component } from '@angular/core';
-import { DomSanitizer } from '@angular/platform-browser';
-import { MatIconRegistry } from '@angular/material';
+import { Component }    from '@angular/core';
+import navbarRoutes             from './../../../../config/routes';
+
+const userType = 'volunteer';
 
 @Component({
     selector: 'navigation',
@@ -10,26 +11,10 @@ import { MatIconRegistry } from '@angular/material';
 
 export class NavigationComponent {
 
-    public pages = [
-        {
-            "name": "Dashboard"
-        },
-        {
-            "name": "Inventory"
-        },
-        {
-            "name": "Allocation"
-        },
-        {
-            "name": "Packing List"
-        }
-    ]
+    public pages;
 
-    constructor (iconRegistry: MatIconRegistry, sanitizer: DomSanitizer) {
-        iconRegistry.addSvgIcon('Dashboard', sanitizer.bypassSecurityTrustResourceUrl('assets/images/dashboard.svg'));
-        iconRegistry.addSvgIcon('Inventory', sanitizer.bypassSecurityTrustResourceUrl('assets/images/inventory.svg'));
-        iconRegistry.addSvgIcon('Allocation', sanitizer.bypassSecurityTrustResourceUrl('assets/images/allocation.svg'));
-        iconRegistry.addSvgIcon('Packing List', sanitizer.bypassSecurityTrustResourceUrl('assets/images/packlist.svg'));
+    constructor () {
+        this.pages = navbarRoutes[userType];
     }
 
 }
---
+++ frontend/src/config/routes.ts
@@ -0,0 +1,50 @@
+const navbarRoutes = {
+    "admin": [
+        {
+            "name": "Dashboard",
+            "route": "/admin/dashboard"
+        },
+        {
+            "name": "Inventory",
+            "route": "/admin/inventory"
+        },
+        {
+            "name": "Allocation",
+            "route": "/admin/allocation"
+        },
+        {
+            "name": "Packing List",
+            "route": "/admin/packing-list"
+        }
+    ],
+    "volunteer": [
+        {
+            "name": "Dashboard",
+            "route": "/volunteer/dashboard"
+        },
+        {
+            "name": "Inventory",
+            "route": "/volunteer/inventory"
+        },
+        {
+            "name": "Packing List",
+            "route": "/volunteer/packing-list"
+        }
+    ],
+    "beneficiary": [
+        {
+            "name": "Dashboard",
+            "route": "/beneficiary/dashboard"
+        },
+        {
+            "name": "Inventory",
+            "route": "/beneficiary/inventory"
+        },
+        {
+            "name": "Request",
+            "route": "/beneficiary/request"
+        }
+    ]
+};
+
+export default navbarRoutes;
