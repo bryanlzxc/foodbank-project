7cbae9bee59dac91bfe5fcc4fd1284c0f270aea1
bryanlzxc <32081492+bryanlzxc@users.noreply.github.com>
2017-12-17 21:28:29 +0800

    Fixing of InventorySerializer null case

--- backend/foodbank/src/main/java/foodbank/util/InventorySerializer.java
+++ backend/foodbank/src/main/java/foodbank/util/InventorySerializer.java
@@ -22,20 +22,12 @@ public class InventorySerializer {
 	@Autowired
 	private FoodRepository foodRepository;
 	
-	@Transient
-	private static Map<String, UUID> transientSerials;
-	
-	public static final Map<String, UUID> serials = transientSerials;
-	
-	@Transient
-	private static Map<UUID, FoodItem> transientFoodItemMap;
-	
-	public static final Map<UUID, FoodItem> foodItemMap = transientFoodItemMap;
+	public static final Map<String, UUID> serials = new HashMap<String, UUID>();
+		
+	public static final Map<UUID, FoodItem> foodItemMap = new HashMap<UUID, FoodItem>();
 	
 	@PostConstruct
 	private void initializeSerials() {
-		Map<String, UUID> serials = new HashMap<String, UUID>();
-		Map<UUID, FoodItem> foodItemMap = new HashMap<UUID, FoodItem>();
 		List<Category> categories = foodRepository.findAll();
 		for(Category category : categories) {
 			List<Classification> classifications = category.getClassification();
@@ -44,13 +36,11 @@ public class InventorySerializer {
 				for(FoodItem foodItem : foodItems) {
 					String identifierKey = category.getCategory() + classification.getClassification() + foodItem.getDescription();
 					UUID serialNo = UUID.randomUUID();
-					serials.put(identifierKey, serialNo);
-					foodItemMap.put(serialNo, foodItem);
+					InventorySerializer.serials.put(identifierKey, serialNo);
+					InventorySerializer.foodItemMap.put(serialNo, foodItem);
 				}
 			}
 		}
-		transientSerials = serials;
-		transientFoodItemMap = foodItemMap;
 	}
 
 }
